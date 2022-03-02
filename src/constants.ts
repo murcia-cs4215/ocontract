657ac2c16ab983b47c94243939eafbf4ce870f13
Hanming Zhu <hanming@huawen.com>
2022-03-02 16:05:59 +0800

    Add unknown location constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,13 @@
+import { SourceLocation } from 'parser/types';
+
 export const GLOBAL = typeof window === 'undefined' ? global : window;
+export const UNKNOWN_LOCATION: SourceLocation = {
+  start: {
+    line: -1,
+    column: -1,
+  },
+  end: {
+    line: -1,
+    column: -1,
+  },
+};
