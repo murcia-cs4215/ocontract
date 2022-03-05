45ed36993444bc5dc688669129fec52d9f0e9d47
Hanming Zhu <hanming@huawen.com>
2022-03-05 13:33:04 +0800

    Add tests for functions

---
+++ src/__tests__/week7/functions.test.ts
@@ -0,0 +1,72 @@
+import { runTest } from 'utils/tests';
+
+import { intType } from '../../constants';
+
+test('single parameter function', () => {
+  const res = runTest('let x a = a + 10;;');
+  expect(res.status).toBe('finished');
+
+  // TODO: Add test for type once typing is introduced
+});
+
+test('multiple parameter function', () => {
+  const res = runTest('let x a b = a + b;;');
+  expect(res.status).toBe('finished');
+
+  // TODO: Add test for type once typing is introduced
+});
+
+// TODO: Add more comprehensive tests once recursive functionality is fully supported
+test('recursive function', () => {
+  const res = runTest('let rec x a = a;;');
+  expect(res.status).toBe('finished');
+
+  // TODO: Add test for type once typing is introduced
+});
+
+test('function call with one argument', () => {
+  const res = runTest(`
+    let rec x a = a + 10;;
+    x 20;;
+  `);
+  expect(res).toEqual({
+    status: 'finished',
+    value: 30,
+    type: intType,
+  });
+});
+
+test('function call with two arguments', () => {
+  const res = runTest(`
+    let x a b = a + b;;
+    x 20 10;;
+  `);
+  expect(res).toEqual({
+    status: 'finished',
+    value: 30,
+    type: intType,
+  });
+});
+
+test('function currying', () => {
+  const res = runTest(`
+    let x a b = a + b;;
+    let y = x 10;;
+  `);
+  expect(res.status).toBe('finished');
+
+  // TODO: Add test for type once typing is introduced
+});
+
+test('function currying with call', () => {
+  const res = runTest(`
+    let x a b = a + b;;
+    let y = x 10;;
+    y 20;;
+  `);
+  expect(res).toEqual({
+    status: 'finished',
+    value: 30,
+    type: intType,
+  });
+});
