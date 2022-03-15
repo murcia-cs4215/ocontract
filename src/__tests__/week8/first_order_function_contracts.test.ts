758ae0b93cec2285431f07cf5cafb01c4cfb706e
raysonkoh <rayson.rk.koh@gmail.com>
2022-03-15 18:22:26 +0800

    Add more contract tests

--- src/__tests__/week8/first_order_function_contracts.test.ts
+++ src/__tests__/week8/first_order_function_contracts.test.ts
@@ -1,4 +1,5 @@
 import { runTest } from 'utils/tests';
+import { intType } from 'utils/typing';
 
 test('argument contract violation', () => {
   const res = runTest(`
@@ -48,6 +49,71 @@ test('curried functions propogate contracts', () => {
     let f x y = x + y;;
     let g = f 1;;
     g (-2);;
+  `);
+  expect(res).toEqual({
+    status: 'errored',
+  });
+});
+
+test('contract violation for function contracts using alternate syntax', () => {
+  const res = runTest(`
+    contract f = {x | x > 0} -> {y | y > 0} -> {z | z > 0};;
+    let f x y = x + y;;
+    let g = f 1;;
+    g (-2);;
+  `);
+  expect(res).toEqual({
+    status: 'errored',
+  });
+});
+
+test('contract satisfied for function contracts using alternate syntax', () => {
+  const res = runTest(`
+    contract f = {x | x > 0} -> {y | y > 0} -> {z | z > 0};;
+    let f x y = x + y;;
+    let g = f 1;;
+    g 1;;
+  `);
+  expect(res).toEqual({
+    status: 'finished',
+    value: 2,
+    type: intType,
+  });
+});
+
+test('can detect precondition violation for complex contracts', () => {
+  const res = runTest(`
+    let notEqTriple a b c = fun d -> d != a && d != b && d != c;;
+    contract f = (notEqTriple 1 2 3) -> (notEqTriple 1 2 3) -> (notEqTriple 1 2 3)
+    let f x y = x + y;;
+    let g = f 1;;
+    g 1;;
+  `);
+  expect(res).toEqual({
+    status: 'errored',
+  });
+});
+
+test('can detect postcondition violation for complex contracts', () => {
+  const res = runTest(`
+    let notEqTriple a b c = fun d -> d != a && d != b && d != c;;
+    contract f = (notEqTriple 1 2 3) -> (notEqTriple 1 2 3) -> (notEqTriple 1 2 3)
+    let f x y = x + y;;
+    let g = f 4;;
+    g (-1);;
+  `);
+  expect(res).toEqual({
+    status: 'errored',
+  });
+});
+
+test('contract satisfied for complex contracts', () => {
+  const res = runTest(`
+    let notEqTriple a b c = fun d -> d != a && d != b && d != c;;
+    contract f = (notEqTriple 1 2 3) -> (notEqTriple 1 2 3) -> (notEqTriple 1 2 3)
+    let f x y = x + y;;
+    let g = f 4;;
+    g 4);;
   `);
   expect(res).toEqual({
     status: 'errored',
