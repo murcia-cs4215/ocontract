7f177304601b1f0cc2941cfd4c314ca441309709
raysonkoh <rayson.rk.koh@gmail.com>
2022-04-08 04:40:30 +0800

    Add fix for pred contracts wrong blame

--- src/__tests__/week8/pred_contracts.test.ts
+++ src/__tests__/week8/pred_contracts.test.ts
@@ -5,7 +5,7 @@ test('contract violation on assignment', () => {
   const res = runTest(`let gt0 : int -> bool = fun (x : int) : bool -> x > 0;;
 contract f = gt0;;
 let f : int = -1;;`);
-  expectContractViolation(res, 'f', 2, 0); // contract definition location
+  expectContractViolation(res, 'main', 2, 0); // contract definition location
 });
 
 test('no contract violation if satisfy contract', () => {
@@ -24,7 +24,7 @@ test('contract violation on complex expression', () => {
   const res = runTest(`let gt0 : int -> bool = fun (x : int) : bool -> x > 0;;
 contract f = gt0;;
 let f : int = if false then 1 + 3 else (-1) - 100;;`);
-  expectContractViolation(res, 'f', 2, 0); // contract definition location
+  expectContractViolation(res, 'main', 2, 0); // contract definition location
 });
 
 test('no contract violation on complex expression if satisfy contract', () => {
@@ -49,5 +49,45 @@ let f : int = if true then 1 + 3 else (-1) - 100;;`);
     value: 4,
     type: intType,
     name: 'f',
+  });
+});
+
+test('contract violation in let local binding LHS', () => {
+  const res =
+    runTest(`let gt (x : int) : int -> bool = fun (y : int) : bool -> y > x;;
+contract f = (gt 0);;
+let f : int = 0 in f + 1;;`);
+  expectContractViolation(res, 'main', 2, 0); // contract definition location
+});
+
+test('no contract violation in let local binding LHS', () => {
+  const res =
+    runTest(`let gt (x : int) : int -> bool = fun (y : int) : bool -> y > x;;
+contract f = (gt 0);;
+let f : int = 1 in f + 1;;`);
+  expect(res).toEqual({
+    status: 'finished',
+    value: 2,
+    type: intType,
+  });
+});
+
+test('contract violation in let local binding RHS', () => {
+  const res =
+    runTest(`let gt (x : int) : int -> bool = fun (y : int) : bool -> y > x;;
+contract f = (gt 0);;
+let x : int = 0 in let f : int = 0 in 5;;`);
+  expectContractViolation(res, 'main', 2, 0); // contract definition location
+});
+
+test('no contract violation in let local binding RHS', () => {
+  const res =
+    runTest(`let gt (x : int) : int -> bool = fun (y : int) : bool -> y > x;;
+contract f = (gt 0);;
+let x : int = 0 in let f : int = 1 in 5;;`);
+  expect(res).toEqual({
+    status: 'finished',
+    value: 5,
+    type: intType,
   });
 });
