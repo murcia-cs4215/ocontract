c709c075af443817691fa72afb51566efa320fde
Hanming Zhu <hanming@huawen.com>
2022-04-14 02:49:54 +0800

    Add more tests

--- src/__tests__/week11/complex_hof_contracts.test.ts
+++ src/__tests__/week11/complex_hof_contracts.test.ts
@@ -1,6 +1,7 @@
 import { expectContractViolation, runTest } from 'utils/tests';
 
 test('function with hof contract violating its own contract', () => {
+  // f should not pass in a negative number into a since its own contract restricts x > 0
   const res =
     runTest(`contract f = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
 let f (a : int -> int) : int = a (-10);;
@@ -15,6 +16,7 @@ f g;;`);
 });
 
 test("function with hof contract violating that function's contract", () => {
+  // f is doing what is supposed to be correct, but this violates g's contract, so main is at fault here
   const res =
     runTest(`contract f = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
 let f (a : int -> int) : int  = a 10;;
@@ -26,4 +28,49 @@ f g;;`);
 
   // Blame main for passing in a function with a different contract to us.
   expectContractViolation(res, 'main', 4, 0);
+});
+
+test('function with hof contract being passed in a function that violates its own contract (but not the hof contract)', () => {
+  // f is behaving properly, but g violates its own contract
+  const res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | y < 0}) -> {z : int | z < 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract g = {x : int | x > 0} -> {y : int | y > 0};;
+let g (a : int) : int = -a;;
+
+f g;;`);
+
+  // Blame g for violating its own contract
+  expectContractViolation(res, 'g', 4, 0);
+});
+
+test("function with hof contract that returns a result violating this function's hof return contract", () => {
+  // f expects y > 0 from its hof arg but g gives y < 0
+  const res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract g = {x : int | x > 0} -> {y : int | y < 0};;
+let g (a : int) : int = -a;;
+
+f g;;`);
+
+  // Blame main for passing in a function with a different contract to us.
+  expectContractViolation(res, 'main', 1, 0);
+});
+
+test("function with hof contract that returns a result violating this function's return value contract", () => {
+  // f expects z > 0 from its hof arg but g gives z < 0
+  const res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | true}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract g = {x : int | x > 0} -> {y : int | y < 0};;
+let g (a : int) : int = -a;;
+
+f g;;`);
+
+  // Blame main for passing in a function with a different contract to us.
+  expectContractViolation(res, 'main', 1, 0);
 });
