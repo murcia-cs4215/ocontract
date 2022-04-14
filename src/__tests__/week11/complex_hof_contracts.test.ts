3ef2d1d17eb7690ff09f02f902fd129db54c9701
Hanming Zhu <hanming@huawen.com>
2022-04-14 13:20:32 +0800

    Add tests with 2 levels of hof passing

--- src/__tests__/week11/complex_hof_contracts.test.ts
+++ src/__tests__/week11/complex_hof_contracts.test.ts
@@ -74,3 +74,105 @@ f g;;`);
   // Blame f for not having sufficiently strict conditions
   expectContractViolation(res, 'f', 1, 0);
 });
+
+test('hof passed twice with violation on original contract', () => {
+  // Violate original parameter contract
+  let res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract h = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let h (b : int -> int) : int = f b;;
+
+contract g = {x : int | x < 0} -> {y : int | y > 0};;
+let g (a : int) : int = -a;;
+
+h g;;`);
+
+  // Blame main for passing in an inappropriate function
+  expectContractViolation(res, 'main', 7, 0);
+
+  // Violate original return value contract
+  res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract h = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let h (b : int -> int) : int = f b;;
+
+contract g = {x : int | x > 0} -> {y : int | y < 0};;
+let g (a : int) : int = a;;
+
+h g;;`);
+
+  // Blame g for violating its own contract
+  expectContractViolation(res, 'g', 7, 0);
+});
+
+test("hof passed twice with violation on first function's contract", () => {
+  // Violate h's hof parameter contract
+  let res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract h = ({x : int | x < 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let h (b : int -> int) : int = f b;;
+
+contract g = {x : int | x > 0} -> {y : int | y > 0};;
+let g (a : int) : int = a;;
+
+h g;;`);
+
+  // Blame h for passing b into f knowing that the contracts don't match up
+  expectContractViolation(res, 'h', 4, 0);
+
+  // Violate h's hof return value contract
+  res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract h = ({x : int | x > 0} -> {y : int | y < 0}) -> {z : int | z > 0};;
+let h (b : int -> int) : int = f b;;
+
+contract g = {x : int | x > 0} -> {y : int | y > 0};;
+let g (a : int) : int = a;;
+
+h g;;`);
+
+  // Blame main for passing in g into h, which doesn't fulfil the contract
+  expectContractViolation(res, 'main', 4, 0);
+});
+
+test("hof passed twice with violation on second function's contract", () => {
+  // Violate f's hof parameter contract
+  let res =
+    runTest(`contract f = ({x : int | x < 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract h = ({x : int | x > 0} -> {y : int | y > 0}) -> {z : int | z > 0};;
+let h (b : int -> int) : int = f b;;
+
+contract g = {x : int | x > 0} -> {y : int | y > 0};;
+let g (a : int) : int = a;;
+
+h g;;`);
+
+  // Blame f for violating its own contract
+  expectContractViolation(res, 'f', 1, 0);
+
+  // Violate f's hof return value contract
+  res =
+    runTest(`contract f = ({x : int | x > 0} -> {y : int | y < 0}) -> {z : int | z > 0};;
+let f (a : int -> int) : int  = a 10;;
+
+contract h = ({x : int | x > 0} -> {y : int | y < 0}) -> {z : int | z > 0};;
+let h (b : int -> int) : int = f b;;
+
+contract g = {x : int | x > 0} -> {y : int | y > 0};;
+let g (a : int) : int = a;;
+
+h g;;`);
+
+  // Blame h for passing in a into f, which doesn't fulfil the contract
+  expectContractViolation(res, 'h', 1, 0);
+});
